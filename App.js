be84bb1a25b8811fbc658188975256101a218b60
RYAN <ryancpotts@gmail.com>
2025-03-12 23:06:08 -0500

    sensor audit taking shape

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
-import WalkingMode from './Components/walkingMode.js'; // Assuming WalkingMode is in the same directory
-import styles from './Components/styles.js'; //Importing separated styles
-import Footer from './Components/Footer.js';
-import Header from './Components/Header.js';
+import { ScrollView, View } from 'react-native';
+import { WalkingMode, Footer, Header, styles } from './Components';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Header />
-
       <ScrollView contentContainerStyle={styles.mainContent}>
         <WalkingMode />
       </ScrollView>
-
       <Footer />
     </View>
   );
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-registerRootComponent(App);
+// Register the main component of the app
+try {
+  registerRootComponent(App);
+} catch (error) {
+  console.error('Error registering root component:', error);
+}
+
+// Export components and styles
+export { default as WalkingMode } from './Components/walkingMode';
+export { default as Footer } from './Components/Footer';
+export { default as Header } from './Components/Header';
+export { default as styles } from './Components/styles';
